465cb1dff7e10a789b58d702f801911f8ea832ef
Vladyslav Gusakov <vladyslavgusakov@gmail.com>
2016-02-10 15:15:31 -0500

    Assignment4

---
+++ NavCtrl/AddCompanyViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddCompanyViewController.h
+//  NavCtrl
+//
+//  Created by Vladyslav Gusakov on 2/4/16.
+//  Copyright © 2016 Aditya Narayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCompanyViewController : UIViewController
+
+@end
---
+++ NavCtrl/EditProductViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditProductViewController.h
+//  NavCtrl
+//
+//  Created by Vladyslav Gusakov on 2/10/16.
+//  Copyright © 2016 Aditya Narayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProductViewController : UIViewController
+
+@end
